c483fb2eb1187855b1f271da57ed558061e9fd18
Royal dsouza <royaldsouza2004@gmail.com>
2024-02-16 17:24:28 +0530

    fixed engine

--- data/database.js
+++ data/database.js
@@ -4,5 +4,5 @@ export const connectDB = () => {
 
       mongoose.connect(process.env.MONGO_URI, {
             dbName: "backendapi",
-      }).then(() => console.log("db connected")).catch((e) => console.log(e));
+      }).then((c) => console.log(`Database connected with ${c.connection.host}`)).catch((e) => console.log(e));
 }
