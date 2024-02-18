293497593cdda40927f6a4f8b02122c93d6365df
Royal dsouza <royaldsouza2004@gmail.com>
2024-02-18 15:05:28 +0530

    fixed

--- app.js
+++ app.js
@@ -9,25 +9,27 @@ import cors from "cors";
 export const app = express();
 
 config({
-      path: "./data/config.env"
-})
+  path: "./data/config.env",
+});
 
-//middlewear
+// Using Middlewares
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({
-      origin:[process.env.FRONTENT_URL],
-      methods:["GET" , "POST" , "PUT" , "DELETE"],
-      credentials:true,
-}));
+app.use(
+  cors({
+    origin: [process.env.FRONTEND_URL],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
 
-//importing route
+// Using routes
 app.use("/api/v1/users", userRouter);
-app.use("/api/v1/tasks", taskRouter);
+app.use("/api/v1/task", taskRouter);
 
-app.get('/', (req, res) => {
-      res.send("Nice working");
-})
+app.get("/", (req, res) => {
+  res.send("Nice working");
+});
 
-//using error middleware
+// Using Error Middleware
 app.use(errorMiddleware);
