5c55165974d74614b92302decc81bd1d7c3d37d0
Royal dsouza <royaldsouza2004@gmail.com>
2024-02-18 15:59:25 +0530

    CHECKING

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
       cors({
-            origin: `"${process.env.FRONTENT_URL}"`,
+            origin: "http://localhost:5173",
             methods: ["GET", "POST", "PUT", "DELETE"],
             credentials: true,
       })
